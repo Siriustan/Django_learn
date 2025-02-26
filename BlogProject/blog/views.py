d2ab11f977b98bde90b35530c3b586f4effe8482
Siriustan <374053551@qq.com>
2025-02-26 09:51:43 +0800

    66

--- BlogProject/blog/views.py
+++ BlogProject/blog/views.py
@@ -8,5 +8,6 @@ def index(request):
     post_list = Post.objects.all().order_by('-created_time')
     return render(request,'blog/index.html',context={'post_list':post_list})
 
-def detail(request):
-    pass
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/detail.html', context={'post': post})
