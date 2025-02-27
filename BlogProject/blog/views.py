f7c83272a91f4f980bfeb39d687360d4fb47f392
Siriustan <374053551@qq.com>
2025-02-27 17:44:30 +0800

    0227

--- BlogProject/blog/views.py
+++ BlogProject/blog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
 from .models import Post
+import markdown
+import re
 
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
@@ -10,4 +12,12 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    md = markdown.Markdown(extensions=[
+                                      'markdown.extensions.extra',
+                                      'markdown.extensions.codehilite',
+                                      'markdown.extensions.toc',
+                                  ])
+    post.body = md.convert(post.body)
+    m = re.search(r'<div class="toc">\s*<ul>(.*)</ul>\s*</div>', md.toc, re.S)
+    post.toc = m.group(1) if m is not None else ''
     return render(request, 'blog/detail.html', context={'post': post})
